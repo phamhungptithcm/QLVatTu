df657328fbc11cd0031cd5f8a792710cc56d2af9
Pham Hung <phamhung.pitit@gmail.com>
2018-06-27 10:50:35 +0700

    first lib

--- mylib.h
+++ mylib.h
@@ -1,42 +1,96 @@
 
-#include <conio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <dos.h>
-#include <string.h>
-#include <windows.h>
-#define Enter 13
-const int WHITE=15;
-#define PASSWORD "abcdef"
-
-using namespace std;
-char* Pwd () {
-     char S[40]; int i=0;
-     while ((S[i]= getch()) != Enter ) 
-     { printf ("%c", '*') ; i++ ; 
-     }
-     S[i]='\0';
-     return S;
-}
-
-int CheckPwd () {
-    int dem =0; 
-    for ( dem =1 ; dem <=3 ; dem++)
-    { printf( "Password :"); 
-      if (strcmp(Pwd(),PASSWORD) ==0)   return 1;
-      else printf ( "\nPassword sai. Hay nhap lai\n")  ; 
-    }
-    return 0;  
-}
-
-void gotoxy(short x,short y)
+#include <Windows.h>
+#include<conio.h>
+// Hàm tô màu.
+void textcolor(int x)
 {
-        HANDLE hConsoleOutput;
-        COORD Cursor_an_Pos = { x,y};
-        hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleCursorPosition(hConsoleOutput , Cursor_an_Pos);
-}  
+	HANDLE mau;
+	mau = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(mau, x);
+}
 
+//ham dich chuyen con tro
+void gotoxy(int x, int y)
+{
+	HANDLE hConsoleOutput;
+	COORD Cursor_an_Pos = { x - 1, y - 1 };
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
+}
+void Nocursortype()//an dau nhay
+{
+	CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = FALSE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+
+void Yescursortype()//hien dau nhay
+{
+	CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = TRUE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+// Thiết lập chế độ hiển thị, có fullscreen hay không
+BOOL NT_SetConsoleDisplayMode(HANDLE hOutputHandle, DWORD dwNewMode)
+{
+	typedef BOOL(WINAPI *SCDMProc_t) (HANDLE, DWORD, LPDWORD);
+	SCDMProc_t SetConsoleDisplayMode;
+	HMODULE hKernel32;
+	BOOL bFreeLib = FALSE, ret;
+	const char KERNEL32_NAME[] = "kernel32.dll";
+
+	hKernel32 = GetModuleHandleA(KERNEL32_NAME);
+	if (hKernel32 == NULL)
+	{
+		hKernel32 = LoadLibraryA(KERNEL32_NAME);
+		if (hKernel32 == NULL)
+			return FALSE;
+
+		bFreeLib = true;
+	}
+
+	SetConsoleDisplayMode =
+		(SCDMProc_t)GetProcAddress(hKernel32, "SetConsoleDisplayMode");
+	if (SetConsoleDisplayMode == NULL)
+	{
+		SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+		ret = FALSE;
+	}
+	else
+	{
+		DWORD tmp;
+		ret = SetConsoleDisplayMode(hOutputHandle, dwNewMode, &tmp);
+	}
+
+	if (bFreeLib)
+		FreeLibrary(hKernel32);
+
+	return ret;
+}
+
+// Đặt chế độ FullScreen
+void setFullScreen()
+{
+	NT_SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), 1);
+	
+}
+void clrscr()
+{
+	CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;
+	HANDLE	hConsoleOut;
+	COORD	Home = { 0,0 };
+	DWORD	dummy;
+
+	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hConsoleOut, &csbiInfo);
+
+	FillConsoleOutputCharacter(hConsoleOut, ' ', csbiInfo.dwSize.X * csbiInfo.dwSize.Y, Home, &dummy);
+	csbiInfo.dwCursorPosition.X = 0;
+	csbiInfo.dwCursorPosition.Y = 0;
+	SetConsoleCursorPosition(hConsoleOut, csbiInfo.dwCursorPosition);
+}
 int wherex( void )
 {
     HANDLE hConsoleOutput;
@@ -53,47 +107,4 @@ int wherey( void )
     CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
     GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
     return screen_buffer_info.dwCursorPosition.Y;
-}
-void clreol( ) {
-COORD coord;
-DWORD written;
-CONSOLE_SCREEN_BUFFER_INFO info;
-GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
-coord.X = info.dwCursorPosition.X;
-coord.Y = info.dwCursorPosition.Y;
-FillConsoleOutputCharacter (GetStdHandle(STD_OUTPUT_HANDLE), ' ',
-  info.dwSize.X - info.dwCursorPosition.X * info.dwCursorPosition.Y, coord, &written);
-gotoxy (info.dwCursorPosition.X + 1, info.dwCursorPosition.Y + 1);
-}
-
-void SetColor(WORD color)
-{
-    HANDLE hConsoleOutput;
-    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
-    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
-
-    WORD wAttributes = screen_buffer_info.wAttributes;
-    color &= 0x000f;
-    wAttributes &= 0xfff0;
-    wAttributes |= color;
-
-    SetConsoleTextAttribute(hConsoleOutput, wAttributes);
-}
-void SetBGColor(WORD color)
-{
-    HANDLE hConsoleOutput;
-    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
-    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
-
-    WORD wAttributes = screen_buffer_info.wAttributes;
-    color &= 0x000f;
-    color <<= 4;
-    wAttributes &= 0xff0f;
-    wAttributes |= color;
-
-    SetConsoleTextAttribute(hConsoleOutput, wAttributes);
 }
